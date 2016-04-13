47b42d9175f8b94ea862a135935366d28a866b01
Akira Matsuda <ronnie@dio.jp>
2016-04-14 00:16:39 +0900

    Explicitly require kaminari and mongo_mapper

--- lib/kaminari/mongo_mapper.rb
+++ lib/kaminari/mongo_mapper.rb
@@ -1,7 +1,7 @@
 require "kaminari/mongo_mapper/version"
+require 'kaminari'
+require 'mongo_mapper'
 
-ActiveSupport.on_load :mongo_mapper do
-  require 'kaminari/mongo_mapper/mongo_mapper_extension'
-  ::MongoMapper::Document.send :include, Kaminari::MongoMapperExtension::Document
-  ::Plucky::Query.send :include, Kaminari::PluckyCriteriaMethods
-end
+require 'kaminari/mongo_mapper/mongo_mapper_extension'
+::MongoMapper::Document.send :include, Kaminari::MongoMapperExtension::Document
+::Plucky::Query.send :include, Kaminari::PluckyCriteriaMethods
