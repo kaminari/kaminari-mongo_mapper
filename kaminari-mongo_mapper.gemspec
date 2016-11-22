0c3edf0476c2c370492bdcc4b89f4e80f116e939
Akira Matsuda <ronnie@dio.jp>
2016-11-22 14:32:48 +0900

    RSpec => Test/Unit

--- kaminari-mongo_mapper.gemspec
+++ kaminari-mongo_mapper.gemspec
@@ -23,10 +23,9 @@ Gem::Specification.new do |spec|
   spec.add_dependency 'kaminari-core'
   spec.add_development_dependency "bundler", "~> 1.11"
   spec.add_development_dependency "rake", "~> 10.0"
-  spec.add_development_dependency "minitest", "~> 5.0"
   spec.add_development_dependency 'mongo_mapper'
   spec.add_development_dependency 'rails'
-  spec.add_development_dependency 'rspec-rails', '~> 2.14.1'
+  spec.add_development_dependency 'test-unit-rails'
   spec.add_development_dependency 'capybara'
   spec.add_development_dependency 'database_cleaner'
   spec.add_development_dependency 'rr'
---
+++ test/models/mongo_mapper/mongo_mapper_test.rb
@@ -0,0 +1,88 @@
+# frozen_string_literal: true
+require 'test_helper'
+
+class MongoMapperExtensionTest < ActiveSupport::TestCase
+  setup do
+    begin
+      MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
+      MongoMapper.database = "kaminari_test"
+      class MongoMapperExtensionDeveloper
+        include ::MongoMapper::Document
+        key :salary, Integer
+      end
+    rescue Mongo::ConnectionFailure
+      pending 'can not connect to MongoDB'
+    end
+  end
+
+  setup do
+    MongoMapperExtensionDeveloper.destroy_all
+    41.times { MongoMapperExtensionDeveloper.create!({:salary => 1}) }
+  end
+
+  sub_test_case '#page' do
+    test 'page 1' do
+      developers = MongoMapperExtensionDeveloper.page(1)
+
+      assert_kind_of Plucky::Query, developers
+      assert_equal 1, developers.current_page
+      assert_equal 25, developers.limit_value
+      assert_equal 2, developers.total_pages
+      assert_equal 0, developers.instance_variable_get('@options')[:skip]
+    end
+
+    test 'page 2' do
+      developers = MongoMapperExtensionDeveloper.page(2)
+
+      assert_kind_of Plucky::Query, developers
+      assert_equal 2, developers.current_page
+      assert_equal 25, developers.limit_value
+      assert_equal 2, developers.total_pages
+      assert_equal 25, developers.instance_variable_get('@options')[:skip]
+    end
+
+    test 'page "foobar"' do
+      developers = MongoMapperExtensionDeveloper.page 'foobar'
+
+      assert_kind_of Plucky::Query, developers
+      assert_equal 1, developers.current_page
+      assert_equal 25, developers.limit_value
+      assert_equal 2, developers.total_pages
+      assert_equal 0, developers.instance_variable_get('@options')[:skip]
+    end
+
+    test 'with criteria before' do
+      # should have the proper criteria source
+      assert_equal({:salary => 1}, MongoMapperExtensionDeveloper.where(:salary => 1).page(2).criteria.source)
+
+      developers = MongoMapperExtensionDeveloper.where(:salary => 1).page 2
+
+      assert_equal 2, developers.current_page
+      assert_equal 25, developers.limit_value
+      assert_equal 2, developers.total_pages
+      assert_equal 25, developers.instance_variable_get('@options')[:skip]
+    end
+
+    test 'with criteria after' do
+      # should have the proper criteria source
+      assert_equal({:salary => 1}, MongoMapperExtensionDeveloper.where(:salary => 1).page(2).criteria.source)
+
+      developers = MongoMapperExtensionDeveloper.page(2).where(:salary => 1)
+
+      assert_equal 2, developers.current_page
+      assert_equal 25, developers.limit_value
+      assert_equal 2, developers.total_pages
+      assert_equal 25, developers.instance_variable_get('@options')[:skip]
+    end
+  end
+
+  test '#per' do
+    developers = MongoMapperExtensionDeveloper.page(2).per(10)
+
+    assert_kind_of Plucky::Query, developers
+    assert_equal 2, developers.current_page
+    assert_equal 10, developers.limit_value
+    assert_equal 5, developers.total_pages
+    assert_equal 10, developers.instance_variable_get('@options')[:skip]
+  end
+end
